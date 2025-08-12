903497abb284288b4c14deddaf32e707f59119a4
deepak <projects@365aitech.com>
2025-08-12 14:33:46 +0530

    handling error logs and email service fixes

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger');
 
 // Create transporter
 const createTransporter = () => {
-  return nodemailer.createTransporter({
+  return nodemailer.createTransport ({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: process.env.EMAIL_PORT === '465',
